04747edf416fc7729b9b223e3dd63dfd3901c1c0
Ajit Kumar Sah <ajitk23192@gmail.com>
2026-01-21 10:17:59 +0530

    Add files via upload
    A constructor is a special method that runs automatically when an object is created. It is used to initialize object attributes with initial values, ensuring the object starts in a valid and usable state.

---
+++ SixtyFour.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("Ajit", 19)
+p2 = Person("Nilam", 16)
+print(p1.name, p1.age)
+print(p2.name,p2.age)
