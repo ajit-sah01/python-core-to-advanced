88ca02113061be197bc5524b0ed0d13449828747
Ajit Kumar Sah <ajitk23192@gmail.com>
2026-01-27 09:46:48 +0530

    Add files via upload
    Changing data types converts columns into correct formats such as integers, floats, or strings, ensuring accurate calculations, comparisons, and memory efficiency.

---
+++ TwentyFour.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.DataFrame({"marks": [70, 80, 90]})
+df["grade"] = ["B", "A", "A"]
+# print(df)
+
+df = df.rename(columns={"marks": "Marks"})
+print(df)
+
+df["Marks"] = df["Marks"].astype(float)
+print(df.dtypes)
