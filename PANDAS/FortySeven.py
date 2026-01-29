4e35b00d97bc8714edef080b10bf20dd75233b81
Ajit Kumar Sah <ajitk23192@gmail.com>
2026-01-29 16:50:42 +0530

    Add files via upload
    Aggregation applies summary functions like sum, mean, count, or max on grouped data.

---
+++ PANDAS/FortySeven.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.DataFrame({
+    "Department": ["IT", "IT", "HR", "HR"],
+    "Salary": [50000, 60000, 40000, 45000]
+})
+
+print(df.groupby("Department")["Salary"].sum())
